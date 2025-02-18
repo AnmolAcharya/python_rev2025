3d3585dfc15c7dccda344b21c23d7eeba36885e6
Anmol-Acharya <anmolacharya94@gmail.com>
2025-02-18 10:23:37 -0600

    initial commits

---
+++ countVowels.py
@@ -0,0 +1,17 @@
+def countVowels(words):
+
+    count = 0
+
+    vowel = ['a', 'e', 'i', 'o', 'u']
+
+    for word in words:
+        if word.lower() in vowel:
+            count += 1
+        
+    return count
+
+
+print(countVowels("BANANANA"))
+
+
+
