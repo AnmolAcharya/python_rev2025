286f6d900c54d5d1bc941bc434d1edcbb00bf792
Anmol-Acharya <anmolacharya94@gmail.com>
2025-02-18 23:47:15 -0600

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,66 @@
+# def dictionary(dict):
+
+#     # dict = {"red":3,
+#     #         "bluw": 4,
+#     #         "green": 5,
+#     #         "purple":6}
+    
+
+#     # return dict.keys()
+#     # return list(dict.keys())[0] 
+
+
+#     # we coulkd use iter as well for this --->
+#     return next(iter(dict))
+
+# print(dictionary({"red":3,
+#                   "blue":4,
+#                   "green":5
+#                   }))
+
+####################################################################################################################
+# # from itertools import islice
+# from itertools import islice
+
+
+# def dictionary(dic):
+#     dic = {"gasvd": 2}
+
+#################Get the second element
+
+# def dictionary(d):
+
+#     # iterator = iter(d)
+
+#     # next(iterator)
+#     # return next(iterator)
+
+#     secondKey = iter(d)
+#     next(secondKey)
+#     next(secondKey)
+
+#     return next(secondKey)
+
+
+# print(dictionary({"red":4,
+#                   "blue":5,
+#                   "green":6}))
+
+
+########################################################
+
+# def dictionary(dics):
+
+#     #return list(dics.keys())[2] #starightforward way--->
+
+
+#     iterators = iter(dics)
+#     next(iterators)
+#     next(iterators)
+#     return next(iterators)
+
+# print(dictionary({"red":5,
+#                   "blue":8,
+#                   "yellow":9,
+#                   "green":10
+#                   }))
