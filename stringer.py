94be4cba14c23f14edd6bf296e5bf12147028224
Anmol-Acharya <anmolacharya94@gmail.com>
2025-02-19 09:37:28 -0600

    strings

---
+++ stringer.py
@@ -0,0 +1,17 @@
+def stringer(word):
+
+    print(word[4])
+    
+    # return word.split(' ')
+    return word.replace('Nepal', 'Argentina')
+
+word = "Vamos Nepal"
+
+print(stringer(word))
+
+
+num = 9
+
+str_num = str(num)
+print(str_num)
+
