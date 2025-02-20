c4f9e8ae17a2892dcf3d012c1aebc7344d0596d5
Anmol-Acharya <anmolacharya94@gmail.com>
2025-02-20 08:28:17 -0600

    star.patters

---
+++ star.py
@@ -0,0 +1,44 @@
+# def starPattern():
+
+#     for i in range(1,11):
+#         print(i)
+
+# starPattern()
+
+#############################Nested Loops logic########################
+
+# def starPattern():
+
+#     for i in range(1,6):
+#         for j in range(i):
+#             print("*", end= " ")
+#         print()
+
+# starPattern()
+
+######################################################################
+
+# def starPattern():
+
+#     for i in range(25,0,-1): #start,stop,step
+#         for j in range(i):
+#             print("*", end=" ")
+#         print()
+
+# starPattern()
+
+######################################################################
+
+#STAR Pattern
+
+def startPattern():
+
+    for i in range(1,6):
+        for j in range(i):
+            print("*", end =" ")
+        print()
+
+startPattern()
+
+
+
