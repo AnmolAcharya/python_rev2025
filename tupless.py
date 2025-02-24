36156b98944fb01f1060c6177694db22842a4467
Anmol-Acharya <anmolacharya94@gmail.com>
2025-02-24 09:13:42 -0600

    tuple issues ad list

---
+++ tupless.py
@@ -0,0 +1,16 @@
+tupleses = (1,2,3,4,5)
+
+print(tupleses)
+print(tupleses[0])
+print(tupleses[1])
+
+# tupleses[0] = 1
+
+list_tuple = list(tupleses)
+
+print(list_tuple)
+
+list_tuple[2] = 69
+
+print(list_tuple)
+
