73201ecbfbaa4dd6bb23708d7b629f960e6be670
Anmol-Acharya <anmolacharya94@gmail.com>
2025-02-25 10:17:19 -0600

    2sum

---
+++ 2sum.py
@@ -0,0 +1,8 @@
+# nums = [1,2,4,,5]
+
+# for i in range(len(nums)):
+#     for j in range(i):
+#         if (target == nums[i]+ nums[j]):
+#             return [i,j]
+
+#########################################################################
