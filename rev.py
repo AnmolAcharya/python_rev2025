d5552cc4da8b2c0b051daeb62666e72feabcd09d
Anmol-Acharya <anmolacharya94@gmail.com>
2025-02-25 00:57:46 -0600

    fixxbuzz

--- rev.py
+++ rev.py
@@ -30,17 +30,36 @@
 
 ##############################################Count Digits of a Number ############################################################
 
-def count_digits(n: int) -> int:
+# def count_digits(n: int) -> int:
 
-    return len((str(abs(n))))
+#     return len((str(abs(n))))
 
-#     if n < 0: 
-#         convert_neg_int = str(n)[1:]
-#         count = len(convert_neg_int)  #<<<<<<<<<<<<<<<<<<<<<<<<<<< THIS IS THE WAY TO DO IT WITHOUT USING ABS()
-#         return count
+# #     if n < 0: 
+# #         convert_neg_int = str(n)[1:]
+# #         count = len(convert_neg_int)  #<<<<<<<<<<<<<<<<<<<<<<<<<<< THIS IS THE WAY TO DO IT WITHOUT USING ABS()
+# #         return count
     
-#     convert_dig = str(n)
-#     return len(convert_dig)
+# #     convert_dig = str(n)
+# #     return len(convert_dig)
 
-print(count_digits(-567899))
+# print(count_digits(-567899))
 
+
+# num = -123
+
+
+# if num > 0:
+#     print(int(str(num)[::-1]))
+# else:
+#     print(-int(str(num)[1:][::-1]))
+
+############# 3 + 8 -> 11 and 1 + 1 -> 2 
+
+num = 444
+
+if num == 0: 
+    print(0)
+else:
+    print(1+(num-1) % 9)
+
+    
