c1a2c3cf578148139f03f98a7961970b4a9fd8cc
Anmol-Acharya <anmolacharya94@gmail.com>
2025-03-19 10:23:58 -0500

    linkedlist stuffs

---
+++ LinkedList.py
@@ -0,0 +1,38 @@
+# class Node:
+
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None 
+
+# def traverseLL(head):
+
+#     while head is not None:
+
+#         print(head.data, end = " ")
+
+#         head = head.next
+
+#     print()
+
+
+# # Driver code
+# def main():
+
+#     # Create a hard-coded linked list:
+#     # 10 -> 20 -> 30 -> 40
+#     head = Node(10)
+#     head.next = Node(20)
+#     head.next.next = Node(30)
+#     head.next.next.next = Node(40)
+
+#     # Example of traversing the node and printing
+
+#     traverseLL(head)
+
+
+# if __name__ == "__main__":
+#     main()
+
+
+
+        
