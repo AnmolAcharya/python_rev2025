2679cf0913eb72838c88b1a6165442d9c254acee
Anmol-Acharya <anmolacharya94@gmail.com>
2025-03-19 11:57:10 -0500

    insert at end

--- LinkedList.py
+++ LinkedList.py
@@ -120,58 +120,65 @@
 
 ##########
 
-class Node:
 
+class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
 
-def insert_at_start(head, new_data):
 
-    #creating a new node with the given data 
+# Given the head of a list and an int, appends
+# a new node at the end and returns the head.
+def append(head, new_data):
+
+    # Create a new node
     new_node = Node(new_data)
 
-    # new_node.next = head #make the next of the new node point to the current head 
+    # If the Linked List is empty, make the 
+    # new node as the head and return
+    if head is None:
+        return new_node
 
-    return new_node
+    # Store the head reference in a temporary variable
+    last = head
 
-def print_list(head):
+    # Traverse till the last node
+    while last.next:
+        last = last.next
 
-    curr = head
+    # Change the next pointer of the last 
+    # node to point to the new node
+    last.next = new_node
 
-    while curr is not None:
-
-        print(f"{curr.data}", end=  " ")
-
-        curr = curr.next
+    # Return the head of the list
+    return head
 
 
-    print()
+# This function prints the contents of the 
+# linked list starting from the head
+def print_list(node):
 
-# Driver code to test the functions
+    while node:
+        print(node.data, end=" ")
+        node = node.next
+
+
+# Driver code
 if __name__ == "__main__":
-    # Create the linked list 2->3->4->5
+
+    # Create a hard-coded linked list: 
+    # 2 -> 3 -> 4 -> 5 -> 6
     head = Node(2)
     head.next = Node(3)
     head.next.next = Node(4)
     head.next.next.next = Node(5)
+    head.next.next.next.next = Node(6)
 
-    # Print the original list
-    print("Original Linked List:", end="")
+    print("Created Linked list is: ", end="")
     print_list(head)
 
-    # Insert a new node at the front of the list
-    print("After inserting Nodes at the front:", end="")
-    data = 1
-    head = insert_at_start(head, data)
+    # Example of appending a node at the end
+    head = append(head, 1)
 
-    # Print the updated list
+    print("\nAfter inserting 1 at the end: ", end="")
     print_list(head)
-
-
-
-
-
-        
-
-        
