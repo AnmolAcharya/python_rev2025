6abb5bee20b90ac6e2e1a8a03264c6e8276f943a
Anmol-Acharya <anmolacharya94@gmail.com>
2025-03-20 23:21:02 -0500

    imp basic binsearch

---
+++ BinarySearch.py
@@ -0,0 +1,15 @@
+def squareNum(num):
+
+    
+
+#     i = 1
+#     while i*i <= num:
+#         if i*i == num:
+#             return True
+#         i += 1
+
+#     return False
+
+# num = 4
+# print(squareNum(num))
+
