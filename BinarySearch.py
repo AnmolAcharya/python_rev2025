48b1c00ba1090d1753a96ad5066c9ff6801e92c7
Anmol-Acharya <anmolacharya94@gmail.com>
2025-03-20 23:26:42 -0500

    binsearch for valid sqaures

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,6 +1,27 @@
 def squareNum(num):
 
-    
+    start, end = 1, num
+
+    if num < 2:
+        return True
+
+    while start <= end:
+        mid = start + (end - start)//2
+
+        if num == mid*mid:
+            return True
+        elif num < mid*mid:
+            end = mid - 1
+        else:
+            start = mid + 1
+
+    return False
+
+num = 18
+
+print(squareNum(num))
+
+
 
 #     i = 1
 #     while i*i <= num:
