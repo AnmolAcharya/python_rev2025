e628a8c0f5ac42e8f7fbc538c17b69604ccd4f3c
Anmol-Acharya <anmolacharya94@gmail.com>
2025-03-21 10:23:05 -0500

    Fibonnacci Number

--- recurrecur.py
+++ recurrecur.py
@@ -1,9 +1,26 @@
-def findFactorial(num):
+# def findFactorial(num):
 
-    if num == 0 or num == 1:
+#     if num == 0 or num == 1:
+#         return 1
+    
+#     return num * findFactorial(num-1)
+    
+# num = 5
+# print(findFactorial(num))\
+
+##################################################33333
+
+def findFibonnaciNum(num):
+
+    if num == 0:
+        return 0
+    
+    if num == 1:
         return 1
     
-    return num * findFactorial(num-1)
+    return findFibonnaciNum(num -1) + findFibonnaciNum(num - 2)
+
     
-num = 5
-print(findFactorial(num))
+num = 6
+print(findFibonnaciNum(num))
+
