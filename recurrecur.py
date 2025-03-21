87f347c3a0e83aed7b1127d3481c307c3ad45870
Anmol-Acharya <anmolacharya94@gmail.com>
2025-03-21 10:57:52 -0500

    sum of digits

--- recurrecur.py
+++ recurrecur.py
@@ -10,17 +10,30 @@
 
 ##################################################33333
 
-def findFibonnaciNum(num):
+# def findFibonnaciNum(num):
+
+#     if num == 0:
+#         return 0
+    
+#     if num == 1:
+#         return 1
+    
+#     return findFibonnaciNum(num -1) + findFibonnaciNum(num - 2)
+
+    
+# num = 4
+# print(findFibonnaciNum(num))
+
+############Sum of digits recursively:
+
+def sumDigits(num):
 
     if num == 0:
         return 0
     
-    if num == 1:
-        return 1
-    
-    return findFibonnaciNum(num -1) + findFibonnaciNum(num - 2)
+    return (num%10) + sumDigits(num // 10)
 
-    
-num = 6
-print(findFibonnaciNum(num))
+num = 123455
+
+print(sumDigits(num))
 
