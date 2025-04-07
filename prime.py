650c9e90b1148dd195e253d7d6784a7c710e48e9
Anmol-Acharya <anmolacharya94@gmail.com>
2025-04-06 21:57:41 -0500

    lc rev

--- prime.py
+++ prime.py
@@ -1,20 +1,23 @@
-def primeChecker(num):
+# def primeChecker(num):
 
-    if num <= 1:
-        return False
+#     if num <= 1:
+#         return False
 
-    if num == 2 or num == 3:
-        return True
+#     if num == 2 or num == 3:
+#         return True
     
-    if num % 2 == 0 and num % 3 == 0:
-        return False
+#     if num % 2 == 0 and num % 3 == 0:
+#         return False
     
-    i = 5
-    while i * i <= num:
-        if num % i == 0 or num % (i + 2) == 0:
-            return False
-        i += 6
-    return True
+#     i = 5
+#     while i * i <= num:
+#         if num % i == 0 or num % (i + 2) == 0:
+#             return False
+#         i += 6
+#     return True
 
 
-print(primeChecker(25))
+# print(primeChecker(25))
+
+
+##trees and then heaps heapify top 'k'th elements type questions >>tree rev basic level comp 
